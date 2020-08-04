d651b53cefea33ecc94af55c0540378ae53f3276
AnnZeller <annazeller@mail.ru>
2020-08-04 12:22:42 +0300

    Updates

---
+++ precache-manifest.187c274a97bf28f85506e4e612461636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4587fe1c553500caea48fa39753ff970",
+    "url": "/color-game/index.html"
+  },
+  {
+    "revision": "9094a0801096ec1808c9",
+    "url": "/color-game/static/css/main.75a02d17.chunk.css"
+  },
+  {
+    "revision": "aa299090931f66a08cf0",
+    "url": "/color-game/static/js/2.8f58b3a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-game/static/js/2.8f58b3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9094a0801096ec1808c9",
+    "url": "/color-game/static/js/main.ba080077.chunk.js"
+  },
+  {
+    "revision": "b822b180503cd7189b1a",
+    "url": "/color-game/static/js/runtime-main.26627553.js"
+  }
+]);
